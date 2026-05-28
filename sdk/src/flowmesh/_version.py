b1d3dcb09546a7ae54740fb4d6907896d13ad09a
Noppanat Wadlom <noppanat.wad@gmail.com>
2026-05-28 11:38:40 +0800

    chore: bump version to 0.1.2
    Synchronize the published package versions, internal first-party pins,
    the SDK runtime version constant, and uv.lock for the v0.1.2
    release-prep branch.
    
    Signed-off-by: Noppanat Wadlom <noppanat.wad@gmail.com>

--- sdk/src/flowmesh/_version.py
+++ sdk/src/flowmesh/_version.py
@@ -3,7 +3,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 _PACKAGE_NAME = "flowmesh-sdk"
-_STATIC_VERSION = "0.1.1"
+_STATIC_VERSION = "0.1.2"
 
 
 def _resolve_version() -> str:
