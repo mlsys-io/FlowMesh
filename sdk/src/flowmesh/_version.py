d6604d00340cd388aea76f4ca523ca5d0eaaa33d
Noppanat Wadlom <noppanat.wad@gmail.com>
2026-06-15 09:06:51 +0800

    chore: bump version to v0.1.3
    Signed-off-by: Noppanat Wadlom <noppanat.wad@gmail.com>

--- sdk/src/flowmesh/_version.py
+++ sdk/src/flowmesh/_version.py
@@ -3,7 +3,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 _PACKAGE_NAME = "flowmesh-sdk"
-_STATIC_VERSION = "0.1.3rc1"
+_STATIC_VERSION = "0.1.3"
 
 
 def _resolve_version() -> str:
