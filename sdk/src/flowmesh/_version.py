5f26b7320b39a26b732dc4d05718b8d824fcd35d
Noppanat Wadlom <noppanat.wad@gmail.com>
2026-07-17 19:29:48 +0800

    chore: release v0.1.6
    Signed-off-by: Noppanat Wadlom <noppanat.wad@gmail.com>

--- sdk/src/flowmesh/_version.py
+++ sdk/src/flowmesh/_version.py
@@ -3,7 +3,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 _PACKAGE_NAME = "flowmesh-sdk"
-_STATIC_VERSION = "0.1.5"
+_STATIC_VERSION = "0.1.6"
 
 
 def _resolve_version() -> str:
